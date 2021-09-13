2106e662b3d80fb8109fcaafa2c9b882f3f0c5b7
Jonathan Martz <git@jmartz.de>
2021-09-13 21:30:47 +0200

    add nav content

---
+++ src/components/Nav/Content.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="vc-nav-content" v-if="toggle">
+    <div class="container mb-2">
+      <div class="row">
+        <div class="col-12 col-md-4 col-lg-3">
+          <router-link to="/impressum" class="btn btn-primary d-block"
+            >Impressum</router-link
+          >
+        </div>
+        <div class="col-12 col-md-4 col-lg-3">
+          <router-link to="/impressum" class="btn btn-primary d-block"
+            >Impressum</router-link
+          >
+        </div>
+        <div class="col-12 col-md-4 col-lg-3">
+          <router-link to="/impressum" class="btn btn-primary d-block"
+            >Impressum</router-link
+          >
+        </div>
+        <div class="col-12 col-md-4 col-lg-3">
+          <router-link to="/impressum" class="btn btn-primary d-block"
+            >Impressum</router-link
+          >
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "NavContent",
+    data() {
+        return {
+            
+        }
+    },
+    computed: {
+        toggle: function() {
+            return this.$store.state.nav.toggle;
+        }
+    }
+}
+</script>
