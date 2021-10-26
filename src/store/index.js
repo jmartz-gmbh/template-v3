156cf807875614f55c0d3ed48adcad5d636b9a43
Jonathan Martz <git@jmartz.de>
2021-10-26 19:54:31 +0200

    add seo store

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import { createStore } from 'vuex'
 import moduleBreadcrumb from '@/store/modules/breadcrumb.js';
 import moduleNav from '@/store/modules/nav.js';
+import moduleSeo from '@/store/modules/seo.js';
 import moduleMessages from '@/store/modules/messages.js';
 
 const store = createStore({
     modules: {
         nav: moduleNav,
+        seo: moduleSeo,
         messages: moduleMessages,
         breadcrumb: moduleBreadcrumb,
     }
--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -7,3 +7,19 @@
     </div>
   </div>
 </template>
+<script>
+export default {
+  name: "Homepage",
+  data() {
+    return {
+      
+    }
+  },
+  mounted() {
+    this.$store.commit('seo-prepare', {
+      title: 'Startseite',
+      description: `Willkommen auf der neuen Webseite.`
+    });
+  },
+}
+</script>
